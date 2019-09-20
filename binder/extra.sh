328058d9c1d13171f832bd8a1af1aedc7d012799
Harald Schilly <harald@schil.ly>
2019-09-20 12:43:38 +0000

    webquiz has an installer, interesting

--- binder/extra.sh
+++ binder/extra.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # https://mybinder.readthedocs.io/en/latest/config_files.html#postbuild-run-code-after-installing-the-environment
+# http://mirror.kumi.systems/ctan/macros/latex/contrib/webquiz/doc/webquiz.pdf
 echo "running postBuild"
 set -ex
 
-TARGET=$(kpsewhich -var-value TEXMFLOCAL)
-mkdir -p "$TARGET"
-cd "$TARGET"
-wget http://mirrors.ctan.org/macros/latex/contrib/webquiz.zip
+#TARGET=$(kpsewhich -var-value TEXMFLOCAL)
+cd /tmp
+wget -q http://mirrors.ctan.org/macros/latex/contrib/webquiz.zip
 unzip webquiz.zip
 rm webquiz.zip
-mktexlsr
+cd webquiz
+./scripts/webquiz --tex-install
+#mktexlsr
